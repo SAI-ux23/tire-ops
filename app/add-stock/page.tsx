14f0b82d5c9480a8cd223f0b8437483df2767cfd
SAI-ux23 <arhtays@gmail.com>
2026-02-18 03:51:07 +0700

    Delete app/add-stock directory

--- app/add-stock/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AddStockPage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Add Stock</h1>
-      <p>This is the Add Stock page.</p>
-    </main>
-  );
-}
