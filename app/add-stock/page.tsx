c4dd178ee0414ee55c198771ad9cef8b0836dfc4
SAI-ux23 <arhtays@gmail.com>
2026-02-18 03:55:25 +0700

    Create page.tsx

---
+++ app/add-stock/page.tsx
@@ -0,0 +1,8 @@
+export default function AddStock() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Add Stock Page</h1>
+      <p>This is where you will add new tire stock.</p>
+    </div>
+  );
+}
