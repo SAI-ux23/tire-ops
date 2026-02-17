16883f4d57be86ba4cce34c4e670ec614bd8d872
SAI-ux23 <arhtays@gmail.com>
2026-02-18 03:52:42 +0700

    Delete view-inventory directory

--- view-inventory/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ViewInventoryPage() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>View Inventory</h1>
-      <p>This is the Inventory page.</p>
-    </main>
-  );
-}
