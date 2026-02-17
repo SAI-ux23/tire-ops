ea977024f502726781120f73372663ed689fa819
SAI-ux23 <arhtays@gmail.com>
2026-02-18 03:46:06 +0700

    Create page.tsx

---
+++ app/record-sale/view-inventory/page.tsx
@@ -0,0 +1,8 @@
+export default function ViewInventoryPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>View Inventory</h1>
+      <p>This is the Inventory page.</p>
+    </main>
+  );
+}
